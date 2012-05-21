d237f2c20fbc17bf537f289848c1ac72f5cc7e2f
WouterJ <waldio.webdesign@gmail.com>
2012-05-21 22:47:38 +0200

    Created autoloader for loading classes and other files

---
+++ src/OOPbuilder/Autoloader.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OOPbuilder;
+
+class Autoloader
+{
+    protected $classes;
+    protected $basepath = __DIR__;
+
+    public function setBasepath($basepath)
+    {
+        $this->basepath = (!in_array(substr($basepath, -1), array('/', '\\'))
+                            ? $basepath.DIRECTORY_SEPARATOR
+                            : $basepath
+                          );
+    }
+
+    public function set($class)
+    {
+        $this->classes = $class;
+    }
+
+    public function get($class)
+    {
+        if (!isset($this->classes[$class])) {
+            throw new \InvalidArgumentException('The class '.$class.' is not found in Autloader.');
+        }
+
+        return $this->classes[$class];
+    }
+
+    public function run()
+    {
+        foreach ($this->classes as $class) {
+            require_once $this->basepath.'OOPbuilder'.DIRECTORY_SEPARATOR.$class.'.php';
+        }
+    }
+}
