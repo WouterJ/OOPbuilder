f27e9ed5caa5db34719a96f3daf93c7935fb32d7
WouterJ <waldio.webdesign@gmail.com>
2012-05-29 22:44:01 +0200

    Created file phpdocs

--- src/OOPbuilder/Builder/BuilderInterface.php
+++ src/OOPbuilder/Builder/BuilderInterface.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * This file is part of the OOPbuilder project
+ *
+ * @author Wouter J <http://wouterj.nl>
+ * @license Creative Commons Attribution Share-Alike <http://creativecommons.org/licenses/by-sa/3.0/>
+ */
 
 namespace OOPbuilder\Builder;
 
