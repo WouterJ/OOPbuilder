a9b124cf230aa9817fa69c99005cd207c70ad676
WouterJ <waldio.webdesign@gmail.com>
2012-06-05 20:42:06 +0200

    Created Console use

---
+++ oopbuilder.php
@@ -0,0 +1,13 @@
+#!E:\wouter\web\wamp\bin\php\php5.3.5\php.exe
+<?php
+/**
+ * This file is part of the OOPbuilder project
+ *
+ * @author Wouter J <http://wouterj.nl>
+ * @license Creative Commons Attribution Share-Alike <http://creativecommons.org/licenses/by-sa/3.0/>
+ */
+
+require_once __DIR__.DIRECTORY_SEPARATOR.'src/OOPbuilder/bootstrap.php';
+
+$container['oopbuilder'];
+
